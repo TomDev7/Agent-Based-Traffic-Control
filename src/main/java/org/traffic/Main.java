a9e7bacb257f9e3f0f0a37395bfbed56e8262459
jniewins <j.niewinski@stud.elka.pw.edu.pl>
2021-01-22 23:45:45 +0100

    extended graph

--- src/main/java/org/traffic/Main.java
+++ src/main/java/org/traffic/Main.java
@@ -4,6 +4,8 @@ import akka.actor.typed.ActorRef;
 import akka.actor.typed.ActorSystem;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.ui.view.View;
+import org.graphstream.ui.view.Viewer;
 import org.traffic.actors.MainActor;
 import javax.swing.*;
 
@@ -16,20 +18,18 @@ public class Main {
     ActorRef<String> trafficSystem = ActorSystem.create(MainActor.create(), "MainSystem");
     trafficSystem.tell("start");
 
+//    System.setProperty("org.graphstream.ui", "swing");
+//
+//    Graph graph = new SingleGraph("Tutorial 1");
 
-    System.setProperty("org.graphstream.ui", "swing");
-
-    Graph graph = new SingleGraph("Tutorial 1");
-
-    graph.addNode("A");
-    graph.addNode("B");
-    graph.addNode("C");
-    graph.addEdge("AB", "A", "B");
-    graph.addEdge("BC", "B", "C");
-    graph.addEdge("CA", "C", "A");
-
-    graph.display();
-
+//    graph.addNode("A");
+//    graph.addNode("B");
+//    graph.addNode("C");
+//    graph.addEdge("AB", "A", "B");
+//    graph.addEdge("BC", "B", "C");
+//    graph.addEdge("CA", "C", "A");
+//    Viewer viewer = graph.display();
+//    View view = viewer.getDefaultView();
 
   }
 }
